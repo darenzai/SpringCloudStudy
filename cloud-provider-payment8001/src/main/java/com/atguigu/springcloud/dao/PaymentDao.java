3ccae7c0c11c7edc80e9b6d63244c719d1db34eb
darenzai <1453566224@qq.com>
2021-03-31 00:15:02 +0800

    支付模块构建下

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/dao/PaymentDao.java
@@ -1,10 +1,12 @@
+//PaymentDao
 package com.atguigu.springcloud.dao;
-
 import com.atguigu.springcloud.entities.Payment;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
 @Mapper
 public interface PaymentDao {
-    public  int create(Payment payment);
-    public Payment getPaymentById(@Param("id") Long id);
+    int create(Payment payment);
+
+    Payment getPaymentById(@Param("id") Long id);
 }
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -1,24 +1,18 @@
-package com.atguigu.springcloud.service.impl;
-
+//PaymentServiceImpl
+package com.atguigu.springcloud.service;
 import com.atguigu.springcloud.dao.PaymentDao;
 import com.atguigu.springcloud.entities.Payment;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.Resource;
-
 @Service
-public class PaymentServiceImpl  implements  PaymentDao{
+public class PaymentServiceImpl implements PaymentService{
     @Resource
     private PaymentDao paymentDao;
 
-    @Override
-    public int create(Payment payment) {
+    public int create(Payment payment){
         return paymentDao.create(payment);
     }
-
-    @Override
-    public Payment getPaymentById(Long id) {
+    public Payment getPaymentById(Long id){
         return paymentDao.getPaymentById(id);
     }
 }
