2685b9509faab2435daa600ed68f850eca81a3a6
darenzai <1453566224@qq.com>
2021-04-01 23:32:17 +0800

    2

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/conrtoller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/conrtoller/OrderController.java
@@ -15,17 +15,17 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 
-    public static  final String PAYMENNT_URL="http://localhost:9001";
-
+    //public static  final String PAYMENNT_URL="http://localhost:8001";
+    private final static String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
     @Resource
     private RestTemplate restTemplate;
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
-        return  restTemplate.postForObject(PAYMENNT_URL+"payment/create",payment,CommonResult.class);
+        return  restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
     }
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
-        return  restTemplate.getForObject(PAYMENNT_URL+"payment/get/"+id,CommonResult.class);
+        return  restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
 
 }
